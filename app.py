8eb118855039470879f348b93a0fff38b9c4c58c
Raghu <2raghuvaran@gmail.com>
2025-05-01 15:21:55 +0530

    Fix for Render deployment

--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def index():
             weather = 'error'
     return render_template('index.html', weather=weather)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': 
+    app.run(debug=True, host='0.0.0.0')
+
